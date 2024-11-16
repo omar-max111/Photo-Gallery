2aa4b5e0c14d6968fcc0c91b5c1f95f766912bc6
Omar <omartaji111@gmail.com>
2024-11-17 00:50:57 +0100

    1

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-function upDate(previewPic) {
-    console.log(previewPic); 
-    console.log(previewPic.src, previewPic.alt); 
+document.querySelectorAll(".preview").forEach(img => {
+    img.addEventListener("mouseover", function () {
+        upDate(this);
+    });
+    img.addEventListener("mouseleave", undo);
 
-    
+   
+    img.addEventListener("focus", function () {
+        upDate(this);
+    });
+    img.addEventListener("blur", undo);
+});
+
+
+function upDate(previewPic) {
     const imageDiv = document.getElementById("image");
     imageDiv.style.backgroundImage = `url(${previewPic.src})`;
     imageDiv.innerText = previewPic.alt;
+}
+
+
+function undo() {
+    const imageDiv = document.getElementById("image");
+    imageDiv.style.backgroundImage = "";
+    imageDiv.innerText = "Hover over an image below to display here.";
+}
+
+window.onload = function () {
+    addTabFocusAttributes();
+    console.log("Page loaded, tabindex attributes added.");
+};
+
+
+function addTabFocusAttributes() {
+    const previews = document.querySelectorAll(".preview");
+    previews.forEach((img, index) => {
+        img.setAttribute("tabindex", index + 1); 
+    });
 }
